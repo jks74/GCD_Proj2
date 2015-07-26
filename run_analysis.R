c3578f2fcb3c4eb6d450e9ace693d5172f4e18c4
jks74 <jsawal1@gmail.com>
2015-07-26 17:31:43 -0400

    Create a Tidy Data File

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 library(dplyr)
-
+library(plyr)
 #Download File
 file_url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip" 
 temp <- tempfile()
@@ -7,22 +7,40 @@ download.file(file_url, temp)
 con <- unz(temp, "UCI HAR Dataset.zip")
 unlink(temp)
 
-setwd("UCI_HAR_Dataset")
-act_label <- read.table("./activity_labels.txt")
-features <- read.table("./features.txt")
+act_label <- read.table("./UCI_HAR_Dataset/activity_labels.txt")
+names(act_label) <- c("label","Activity")
+features <- read.table("./UCI_HAR_Dataset/features.txt")
 
-tst_X <- read.table("test/X_test.txt")
-tst_y <- read.table("test/y_test.txt")
-tst_s <- read.table("test/subject_test.txt")
-train_X <- read.table("train/X_train.txt")
-train_y <- read.table("train/y_train.txt")
-train_s <- read.table("train/subject_train.txt")
+#Read Test Data
+tst_X <- read.table("./UCI_HAR_Dataset/test/X_test.txt")
+names(tst_X) <- features$V2
+tst_y <- read.table("./UCI_HAR_Dataset/test/y_test.txt")
+names(tst_y) <- "label"
+tst_s <- read.table("./UCI_HAR_Dataset/test/subject_test.txt")
+names(tst_s) <- "subject"
 
+#Read Training Data
+train_X <- read.table("./UCI_HAR_Dataset/train/X_train.txt")
+names(train_X) <- features$V2
+train_y <- read.table("./UCI_HAR_Dataset/train/y_train.txt")
+names(train_y) <- "label"
 
+train_s <- read.table("./UCI_HAR_Dataset/train/subject_train.txt")
+names(train_s) <- "subject"
+
+#Merge Different columns from Test and Training Sets
 tst <- cbind(tst_X,tst_y,tst_s)
 train <- cbind(train_X,train_y,train_s)
-final <- rbind(train,tst)
 
-# With Header
-final_1 <- 
+#STEP 1: Combine Test/Training Data
+tst_train <- rbind(train,tst)
+#Remove duplicate column names from the data
+final <- tst_train[, unique(colnames(tst_train))]
 
+sm <- select(final,ends_with("std()"),ends_with("mean()"),label,subject)
+#Step 4
+sm$label <- mapvalues(sm$label, from = c(1,2,3,4,5,6), to = c("WALKING", "WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING"))
+
+#Step 5
+td <- ddply(sm, c("subject", "label"), function(x) colMeans(x[c(names(sm)[1:18])]))
+write.table(td,"tidy_data.txt",row.names=FALSE)
